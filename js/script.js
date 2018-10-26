de5cb76975a506290b2200c2b6c6bf61dc39f59d
Enrique <eabucam@gmail.com>
2018-10-26 10:06:43 -0600

    e

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('serviceworker.js')
+	navigator.serviceWorker.register('./serviceworker.js')
 	.then(reg => console.log('Registro de SW Exitoso'))
 	.catch(err => console.warn('Error en registro de SW',err))
 }
